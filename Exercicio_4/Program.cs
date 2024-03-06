21ab6bf13c6f4637d3dd8eb188b9bcc164d2b02a
Filip3ra <filipimacielrj@gmail.com>
2024-03-06 10:28:37 -0300

    exercício 4

---
+++ Exercicio_4/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+  void comSwitch()
+  {
+    Console.WriteLine("Digite um número inteiro:");
+    string? entrada = Console.ReadLine();
+
+    int numero;
+    if (int.TryParse(entrada, out numero))
+    {
+      switch (numero % 2)
+      {
+        case 0:
+          Console.WriteLine("O número é par.");
+          break;
+        case 1:
+          Console.WriteLine("O número é ímpar.");
+          break;
+        default:
+          Console.WriteLine("Algo deu errado. Verifique o número digitado.");
+          break;
+      }
+    }
+    else
+    {
+      Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
+    }
+  }
+
+  void comIfsemElse()
+  {
+    Console.WriteLine("Digite um número inteiro:");
+    string? entrada = Console.ReadLine();
+
+    int numero;
+    if (int.TryParse(entrada, out numero))
+    {
+      if (numero % 2 == 0)
+        Console.WriteLine("O número é par.");
+      if (numero % 2 == 1)
+        Console.WriteLine("O número é ímpar.");
+    }
+    if (int.TryParse(entrada, out numero) == false)
+      Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
+  }
+
+  static void Main()
+  {
+    Program p = new Program();
+    p.comSwitch();
+    p.comIfsemElse();
+
+  }
+}
