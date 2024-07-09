7a71e5fe4d97f57cb83ba12e64b42586fe3dac52
Filip3ra <filipimacielrj@gmail.com>
2024-07-09 18:53:19 -0300

    passando raiva nessa porra

--- Exercicio_10/Program.cs
+++ Exercicio_10/Program.cs
@@ -1,119 +1,16 @@
 ﻿using System;
-using System.Linq;
+using System.Text;
 
-/* Pangrama: Todas as letras do alfabeto são usadas em uma frase,
-   caso tenha algum caractere além dos presentes no alfabeto isso não
-   invalida a frase, mas não ter algum caractere do alfabeto sim.
-*/
-
-namespace PangramaChecker
+class Program
 {
-  // Classe base
-  public abstract class Pangrama
-  {
-    // Protegidos para serem acessíveis nas classes derivadas
-    protected string frase;
-    protected string alfabeto;
-
-    // Como é insensível a maiúsculas e minúsculas, 
-    // podemos converter tudo pra minúscula
-    public Pangrama(string frase)
+    static void Main()
     {
-      this.frase = frase.ToLower();
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        Console.WriteLine("Você digitou:");
+        Console.WriteLine(frase);
     }
-
-    // Verifica se cada letra do alfabeto está na frase
-    public bool VerificaPangrama()
-    {
-      foreach (char letra in alfabeto)
-      {
-        if (!frase.Contains(letra)) // Se não tiver a letra retorna false
-        {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
-
-  public class RemoveAcentos : Pangrama
-  {
-    foreach (char letra in frase)
-    {
-      
-    }
-  }
-
-
-  // Classe derivada para Inglês
-  public class PangramaIngles : Pangrama
-  {
-    public PangramaIngles(string frase) : base(frase)
-    {
-      alfabeto = "abcdefghijklmnopqrstuvwxyz";
-    }
-  }
-
-  // Classe derivada para Português
-  public class PangramaPortugues : Pangrama
-  {
-    public PangramaPortugues(string frase) : base(frase)
-    {
-      alfabeto = "abcdefghijklmnopqrstuvwxyz";
-    }
-  }
-
-  // Antes da reforma ortográfica não tínhamos k,y e w no alfabeto
-  public class PangramaPortuguesAntigo : Pangrama
-  {
-    public PangramaPortuguesAntigo(string frase) : base(frase)
-    {
-      alfabeto = "abcdefghijlmnopqrstuvxz";
-    }
-  }
-
-
-
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Deseja usar em Inglês, Português ou Antigo Português? (I/P/A)");
-      string idioma = Console.ReadLine().ToUpper();
-
-      Console.WriteLine("Digite a frase:");
-      string frase = Console.ReadLine();
-      
-      Pangrama pangramaChecker;
-
-      // Seleciona o idioma
-      if (idioma == "I")
-      {
-        pangramaChecker = new PangramaIngles(frase);
-      }
-      else if (idioma == "P")
-      {
-        pangramaChecker = new PangramaPortugues(frase);
-      }
-      else if (idioma == "A") // Teste português antes do acordo ortográfico
-      {
-        pangramaChecker = new PangramaPortuguesAntigo(frase);
-      }
-      else
-      {
-        Console.WriteLine("Idioma não suportado.");
-        return;
-      }
-
-      // Exibe se é pangrama
-      if (pangramaChecker.VerificaPangrama())
-      {
-        Console.WriteLine("É um pangrama");
-      }
-      else
-      {
-        Console.WriteLine("Não é um pangrama");
-      }
-    }
-  }
 }
